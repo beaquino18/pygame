b313cd37acc77c54f579ebec91955711712bfd6f
Bernadette Aquino <bernadette.p.aquino@gmail.com>
2024-11-11 22:33:00 -0800

    added bomb class

--- game.py
+++ game.py
@@ -56,38 +56,109 @@ class Strawberry(GameObject):
     def reset(self):
         self.x = -64
         self.y = choice(lanes)
+        
 
+class Bomb(GameObject):
+    def __init__(self):
+        super(Bomb, self).__init__(0, 0,'images/bomb.png')
+        self.dx = 0
+        self.dy = 0
+        self.reset()
+        
+    def move(self):
+        self.x += self.dx
+        self.y += self.dy
+        if self.x > 500 or self.x < -64 or self.y > 500 or self.y < -64:
+            self.reset()
+            
+    def reset(self):
+        direction = randint(1, 4)
+        
+        if direction == 1: #left
+            self.x = -64
+            self.y = choice(lanes)
+            self.dx = (randint(0, 200) / 100) + 1
+            self.dy = 0
+        elif direction == 2: #right
+            self.x = 500
+            self.y = choice(lanes)
+            self.dx = (randint(0, 200) / 100) * -1
+            self.dy = 0
+        elif direction == 3: #down
+            self.x = choice(lanes)
+            self.y = -64
+            self.dx = 0
+            self.dy = (randint(0, 200) / 100) + 1
+        else:
+            self.x = choice(lanes)
+            self.y = 500
+            self.dx = 0
+            self.dy = (randint(0, 200) / 100) * -1
+            
+            
+        
 class Player(GameObject):
     def __init__(self):
         super(Player, self).__init__(0, 0, 'images/player.png')
         self.dx = 0
         self.dy = 0
+        self.pos_x = 1 #this is index 1 of lane so, this is 218
+        self.pos_y = 1
         self.reset()
 
+    # Don't allow moving past left edge & move left one lane
     def left(self):
-        self.dx -= 100
+        if self.pos_x > 0:
+            self.pos_x -= 1
+            self.update_dx_dy()
 
+    # Don't allow moving past right edge
     def right(self):
-        self.dx += 100
+        if self.pos_x < len(lanes) - 1:
+            self.pos_x += 1
+            self.update_dx_dy()
 
+    # Don't allow moving past top edge
     def up(self):
-        self.dy -= 100
+        if self.pos_y > 0:
+            self.pos_y -= 1
+            self.update_dx_dy()
 
+    # Don't allow moving past bottom edge
     def down(self):
-        self.dy += 100
+        if self.pos_y < len(lanes) - 1:
+            self.pos_y += 1
+            self.update_dx_dy()
 
     def move(self):
         self.x -= (self.x - self.dx) * 0.25
         self.y -= (self.y - self.dy) * 0.25
 
     def reset(self):
-        self.x = 250 - 32
-        self.y = 250 - 32
+        #Start at center position
+        self.x = lanes[self.pos_x]
+        self.y = lanes[self.pos_y]
+        self.dx = self.x
+        self.dy = self.y
+    
+    def update_dx_dy(self):
+        self.dx = lanes[self.pos_x]
+        self.dy = lanes[self.pos_y]
 
 
 apple = Apple()
 strawberry = Strawberry()
 player = Player()
+bomb = Bomb()
+
+#Make a group
+all_sprites = pygame.sprite.Group()
+# Add sprites to group
+all_sprites.add(player)
+all_sprites.add(apple)
+all_sprites.add(strawberry)
+all_sprites.add(bomb)
+
 
 running = True
 while running:
@@ -107,22 +178,15 @@ while running:
             elif event.key == pygame.K_DOWN:
                 player.down()
     
-    #Clear screen
+    # Clear screen
     screen.fill((255, 255, 255))
     
-    #Draw apple
-    apple.move()
-    apple.render(screen)
-    
-    #Draw strawberries
-    strawberry.move()
-    strawberry.render(screen)
-    
-    #Draw player
-    player.move()
-    player.render(screen)
-    
-    #Update the window
+    # Move and render Sprites
+    for entity in all_sprites:
+        entity.move()
+        entity.render(screen)
+        
+    # Update the window
     pygame.display.flip()
     
     # tick the clock
